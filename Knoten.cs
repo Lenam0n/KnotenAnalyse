318f837c0e13e7ae64059dfd0377dfbb51f3ba1a
Lena <57621352+Lenam0n@users.noreply.github.com>
2023-12-07 14:24:25 +0100

    Add files via upload

--- Knoten.cs
+++ Knoten.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NetzplanAnalyse
+class Knoten
 {
-    internal class Knoten
+    private int Id { get; set; }
+    private string Name { get; set; }
+    private int Dauer { get; set; }
+    private int FEZ { get; set; }
+    private int FAZ { get; set; }
+    private List<Knoten>? Pre { get;set; }
+
+    public Knoten(Knoten k)
     {
+        Pre = new List<Knoten>();
+        Pre.Add(k);
+    }
+    public Knoten()
+    {
+        Pre = null;
+    }    
+    public Knoten(int ID, int Dauer, int FEZ, int FAZ)
+    {
+        Id = ID;
+        Name = Name;
+    }
+
+    public void test()
+    {
+        FEZ = FAZ + Dauer;
     }
 }
+
